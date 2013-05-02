fe2d05d88dfccdedc939fcac553346c992b9a50c
tomas <sourcefly@jun.local>
2013-05-02 21:47:31 +0800

    Initial Commit

---
+++ cocosDargon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cocosDargon
+//
+//  Created by tomas on 13-5-2.
+//  Copyright aiyounet 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
