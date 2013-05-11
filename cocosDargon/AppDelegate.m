b4f6789ec5f8ba7d264eb445e4657b7f87ffb91b
sourcefly <sourcefly1990@gmail.com>
2013-05-11 11:32:48 +0800

    测试用

--- cocosDargon/AppDelegate.m
+++ cocosDargon/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "cocos2d.h"
 
 #import "AppDelegate.h"
-#import "IntroLayer.h"
+#import "CCBReader.h"
 
 @implementation MyNavigationController
 
@@ -47,7 +47,8 @@
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-		[director runWithScene: [IntroLayer scene]];
+        CCScene *sence = [CCBReader sceneWithNodeGraphFromFile:@"WelcomeLayer.ccbi"];
+        [director runWithScene:sence];
 	}
 }
 @end
@@ -123,6 +124,8 @@
 	// Assume that PVR images have premultiplied alpha
 	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
 	
+
+    
 	// Create a Navigation Controller with the Director
 	navController_ = [[MyNavigationController alloc] initWithRootViewController:director_];
 	navController_.navigationBarHidden = YES;
